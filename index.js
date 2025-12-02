c825eb43b08634da5740322fb3e454b34c63f092
Abhishek Anand <abhishek.a23@iiits.in>
2025-12-02 21:14:07 +0530

    pdf upload format changed from cloudinary to multer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 dotenv.config();
 
@@ -40,7 +41,8 @@ app.use(
     origin: [
       process.env.FRONTEND_ORIGIN || "http://localhost:3000",
       "http://localhost:3001",
-      "http://localhost:3002"
+      "http://localhost:3002",
+      "http://localhost:5173"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -50,6 +52,15 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from uploads directory with proper headers
+app.use('/uploads', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
+  next();
+}, express.static(path.join(__dirname, 'uploads')));
 
 app.use(
   session({
