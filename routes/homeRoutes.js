b80f4426469f80daeb5c6bbe52a0e7e088b04932
Deepak-Anuragi <deepak230anuragi@gmail.com>
2026-03-01 04:02:21 +0530

    1 Created the JobApplication.jsx to publicly display the total number of applicants 2 Created the FreelancerProfile.jsx to publicly view the profile of applicants who have applied for the job  3 Blog statistics include the total number of blogs, featured blogs, total views and likes, along with filters and smart search functionality

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -5,6 +5,7 @@ const home = require("../controllers/homeController");
 router.get("/home", home.getHome);
 router.get("/jobs/api", home.getPublicJobs);
 router.get("/jobs/api/:jobId", home.getJobDetail);
+router.get("/jobs/:jobId/applicants", home.getJobApplicants);
 router.get("/freelancer/:freelancerId", home.getFreelancerPublicProfile);
 
 module.exports = router;
